4c6363ef893c6a3239eb3aee8d6af6324ed78cba
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-03-22 19:17:41 -0300

    Doing some refactoring

--- codeforces/1130B.cpp
+++ codeforces/1130B.cpp
@@ -19,7 +19,7 @@ int main() {
     bool isFirst = true;
     for (int i = 1; i < n; i++) {
         ans += min(abs(street[i-1][0] - street[i][0]) + abs(street[i-1][1] - street[i][1]),
-        abs(street[i-1][1] - street[i][0]) + abs(/*  */street[i-1][0] - street[i][1]));
+        abs(street[i-1][1] - street[i][0]) + abs(street[i-1][0] - street[i][1]));
     }
  
     cout << ans << endl;
