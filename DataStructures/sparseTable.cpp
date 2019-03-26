01c0260c5dcf2a26d21226e93f9a5548b4751419
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-03-25 21:58:51 -0300

    Refactoring sparse table

--- DataStructures/sparseTable.cpp
+++ DataStructures/sparseTable.cpp
@@ -6,26 +6,25 @@ using namespace std;
 #define MAXN 100010
 #define ll long long
 
-int spt[MAXL][MAXN], elements[MAXN], n;
+int sparseTable[MAXL][MAXN], elements[MAXN], n;
 
 int log(int n) {
     return __builtin_clz(1) - __builtin_clz(n);
 }
 
 void build() {
-	for(int i = 0; i < n; i++) spt[0][i] = elements[i];
+	for(int i = 0; i < n; i++) sparseTable[0][i] = elements[i];
 
 	for(int j = 1; j <= log(n); j++) {
-		for(int i = 0; (i + (1 << (j-1)) ) < n; i++) {
-			spt[j][i] = min(spt[j-1][i], spt[j-1][i + (1 << (j-1))]);
+		for(int i = 0; (i + (1 << (j-1))) < n; i++) {
+			sparseTable[j][i] = min(sparseTable[j-1][i], sparseTable[j-1][i + (1 << (j-1))]);
 		}
 	}
 }
 
 int query(int l, int r) {
-	int elementosNoIntervalo = r - l + 1;
-    int lg = log(elementosNoIntervalo);
-	return min( spt[ lg ][l], spt[ lg ][ r - (1 << lg) + 1 ] );
+    int lg = log(r - l + 1);
+	return min(sparseTable[lg][l], sparseTable[lg][r - (1 << lg) + 1]);
 }
 
 int main() {
