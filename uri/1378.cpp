b26c6945b046cca09f0724de68d70007c45ad7df
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-09-24 15:51:18 -0300

    Add 1378 from URI

--- uri/1378.cpp
+++ uri/1378.cpp
@@ -1,53 +1,43 @@
 #include <bits/stdc++.h>
 #define MAXN (int)1e4 + 1
-#define pii pair<int, int>
+#define ll long long
+#define pii pair<ll, ll>
 
 using namespace std;
 
-double calc_dist(pii p1, pii p2) {
-  return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
+int n;
+pii points[MAXN];
+int ans;
+
+ll calc_dist(pii p1, pii p2) {
+  ll x = p1.first - p2.first;
+  ll y = p1.second - p2.second;
+  return (x * x) + (y * y);
 }
 
 int main() {
+  ll dist;
 
-  int n;
-  pii points[MAXN];
-  // double dists[MAXN][MAXN];
-  set<pair<pii, int>> ans;
-  map<double, int> waza[MAXN];
-  int x, y, z;
-
-  while (cin >> n && n != 0) {
-    ans.clear();
+  while (scanf("%d", &n) && n != 0) {
+    ans = 0;
+    unordered_map<ll, int> dists;
+    dists.reserve(n);
     for (int i = 0; i < n; i++) {
-      scanf("%d %d", &points[i].first, &points[i].second);
+      scanf("%lld %lld", &points[i].first, &points[i].second);
     }
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
-        double dist = calc_dist(points[i], points[j]);
-        waza[i] = 
+          dist = calc_dist(points[i], points[j]);
+          if (dist > 0) dists[dist]++;
       }
+      for (auto p : dists) {
+        ans += (p.second * (p.second - 1)) / 2.0;
+      }
+      dists.clear();
     }
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        double dist = calc_dist(points[i], points[j]);
-        for (int k = 0; k < n; k++) {
-          if (i != k && k != j && calc_dist(points[i], points[k]) == dist && calc_dist(points[j], points[k]) != dist) {
-            x = min(i, min(j, k));
-            y = max(i, max(j, k));
-            z = (i + j + k) - x - y;
-            ans.insert({{x, y}, z});
-
-            // cout << i << ", " << j << ", " << k << endl;
-            // cout << x << ", " << z << ", " << y << endl;
-          }
-        }
-      }
-    }
-    printf("%d\n", (int)ans.size());
-    // cout << ans.size() << endl;
+    printf("%d\n", ans);
   }
 
   return 0;
